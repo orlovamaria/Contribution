ba6743d295b7d2546140e0398c23ef5853271be4
orlovamaria <arlova.maryia@gmail.com>
2021-08-20 21:54:21 +0300

    Add project files.

---
+++ Contribution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Contribution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Summa = 1000;
+            double Persent = 0.02;
+            int month = 0;
+            
+            while (Summa <= 1200)
+            {
+                double adding = Summa * Persent;
+                Summa += adding;
+                month++;
+            }
+
+            Console.WriteLine($"The amount of the deposit will increase to 1200 in {month} months");
+        }
+    }
+}
